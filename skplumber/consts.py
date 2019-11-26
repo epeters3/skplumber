437f6fb1e642025f2d9a20afa50ab60b25bf311d
epeters3 <evanpeterson17@gmail.com>
2019-11-26 08:53:37 -0700

    Overhaul primitive interfaces

--- skplumber/consts.py
+++ skplumber/consts.py
@@ -1,5 +1,6 @@
 from enum import Enum
 
+
 class ProblemType(Enum):
     CLASSIFICATION = "classification"
     REGRESSION = "regression"
@@ -8,11 +9,8 @@ class ProblemType(Enum):
     def has_value(cls, value):
         return value in set(item.value for item in cls)
 
-class PrimitiveType(Enum):
-    DATA_PREPARATION = 1
-    PREPROCESSOR = 2
-    MODEL =  3
 
-class SKLearnAPIType(Enum):
+class PrimitiveType(Enum):
     TRANSFORMER = 1
-    PREDICTOR = 2
+    REGRESSOR = 2
+    CLASSIFIER = 3
--- skplumber/primitives/primitive.py
+++ skplumber/primitives/primitive.py
@@ -1,40 +1,79 @@
-from typing import Callable, List
+from typing import Callable
+from abc import ABC, abstractmethod
 
 import pandas as pd
 
-from skplumber.consts import ProblemType, PrimitiveType, SKLearnAPIType
+from skplumber.consts import ProblemType, PrimitiveType
 
-class Primitive:
-    def __init__(
-        self,
-        f: Callable,
-        *,
-        primitive_type: PrimitiveType,
-        sklearn_api_type: SKLearnAPIType,
-        supported_problem_types: List[ProblemType],
-    ) -> None:
-        """
-        TODO: Document
-        """
-        self.f = f # The actual underlying sklearn primitive
+
+class Primitive(ABC):
+    """
+    Inherit this class to make a primitive that can work with the
+    skplumber framework.
+    """
+
+    def __init__(self, primitive_type: PrimitiveType) -> None:
         self.primitive_type = primitive_type
-        self.sklearn_api_type = sklearn_api_type
-        self.supported_problem_types = supported_problem_types
-    
-    def fit(self, X: pd.DataFrame, target_name: str):
-        """
-        This method has the same purpose as the sklearn `fit`
-        method, only uses a `target_name` variable as well.
-        Note that `target_name` isn't always used, depending
-        on the underlying sklearn primitive type.
-        """
+        if primitive_type == PrimitiveType.REGRESSOR:
+            self.supported_problem_types = [ProblemType.REGRESSION]
+        elif primitive_type == PrimitiveType.CLASSIFIER:
+            self.supported_problem_types = [ProblemType.CLASSIFICATION]
+        elif primitive_type == PrimitiveType.TRANSFORMER:
+            self.supported_problem_types = [
+                ProblemType.REGRESSION,
+                ProblemType.CLASSIFICATION,
+            ]
+
+    @abstractmethod
+    def fit(self, X, y) -> None:
         pass
-    
-    def produce(self, X: pd.DataFrame, target_name: str):
-        """
-        This method calls the method of the underlying
-        sklearn primitive that gives its output. For
-        transformers its the `transform` method. For
-        predictors its the `predict` method, etc.
-        """
+
+    @abstractmethod
+    def produce(self, X):
         pass
+
+
+def make_sklearn_primitive(sklearn_cls, primitive_type: PrimitiveType):
+    class SKPrimitive(Primitive):
+        f"""
+        An automatically generated `Primitive` implementing wrapper for the
+        '{sklearn_cls.__name__}' class in the `sklearn` package.
+        """
+
+        def __init__(self) -> None:
+            super().__init__(primitive_type)
+            self.sk_primitive = sklearn_cls()
+
+        def fit(self, X: pd.DataFrame, y: pd.Series) -> None:
+            """
+            This method has the same purpose as the sklearn `fit`
+            method. Note that `y` isn't always used, depending
+            on the underlying sklearn primitive type.
+            """
+            self.sk_primitive.fit(X, y)
+
+        def produce(self, X: pd.DataFrame):
+            """
+            This method calls the method of the underlying
+            sklearn primitive that gives its output. For
+            transformers its the `transform` method. For
+            predictors its the `predict` method, etc.
+            """
+            if self.primitive_type in [
+                PrimitiveType.REGRESSOR,
+                PrimitiveType.CLASSIFIER,
+            ]:
+                outputs = self.sk_primitive.predict(X)
+            elif self.primitive_type == PrimitiveType.TRANSFORMER:
+                outputs = self.sk_primitive.transform(X)
+
+            print(outputs.shape)
+            if len(outputs.shape) == 1:
+                # One column output
+                return pd.Series(outputs)
+            else:
+                return pd.DataFrame(outputs)
+
+    SKPrimitive.__name__ = f"{sklearn_cls.__name__}Primitive"
+
+    return SKPrimitive
