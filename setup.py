e09ffc28a18faa1fbdb9f0af556b636d5ab757b6
epeters3 <evanpeterson17@gmail.com>
2019-12-10 16:49:16 -0700

    Bug fixes

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="skplumber",
-    version="0.3.5dev",
+    version="0.3.6dev",
     packages=find_packages(include=["skplumber", "skplumber.*"]),
     license="MIT",
     url="https://github.com/epeters3/skplumber",
