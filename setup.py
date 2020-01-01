d2fc7630bf1f12be4f8b218350a9be18baae99ad
epeters3 <evanpeterson17@gmail.com>
2020-01-01 15:02:01 -0700

    Closes #11 - Add readme to setup.py

--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
     name="skplumber",
-    version="0.4.2dev",
+    version="0.4.3dev",
     packages=find_packages(include=["skplumber", "skplumber.*"]),
     license="MIT",
     url="https://github.com/epeters3/skplumber",
     author="Evan Peterson",
     author_email="evanpeterson17@gmail.com",
     description="A scikit-learn based AutoML tool",
-    # long_description=open("README.md").read(), # TODO: Fix pypi .md parsing bug
-    # long_description_content_type="text/markdown",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
     install_requires=[
         "scikit-learn>=0.21.3",
         "pandas>=0.25.3",
