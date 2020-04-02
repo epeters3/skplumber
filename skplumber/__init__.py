8f4bf2836da61f47952132d6ef45b0a97f1f529c
epeters3 <evanpeterson17@gmail.com>
2020-04-02 17:10:23 -0600

    Ignore more unneeded import warnings

--- skplumber/__init__.py
+++ skplumber/__init__.py
@@ -1,2 +1,2 @@
-from skplumber.skplumber import SKPlumber
-from skplumber.pipeline import Pipeline
+from skplumber.skplumber import SKPlumber  # noqa: F401
+from skplumber.pipeline import Pipeline  # noqa: F401
