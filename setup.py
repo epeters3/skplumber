f378c1d2f51cf5b782bb07f3f8268d20deeac955
epeters3 <evanpeterson17@gmail.com>
2020-04-14 08:24:49 -0600

    Bump version for patch release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="skplumber",
-    version="0.6.0dev",
+    version="0.6.1dev",
     packages=find_packages(include=["skplumber", "skplumber.*"]),
     license="MIT",
     url="https://github.com/epeters3/skplumber",
